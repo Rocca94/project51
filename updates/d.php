345ad74403f83f8e9b8aacdafa4886d286a1338b
Rocca94 <gabriel.roccabruna@gmail.com>
2014-12-09 17:48:50 +0100

    Prova4

--- updates/d.php
+++ updates/d.php
@@ -1,38 +1,27 @@
 <?php
-ini_set('max_execution_time', 4000);
-$iniz=time();
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "orario";
-
-/*$conn = mysqli_connect($servername, $username, $password, $dbname);
+$conn =  new mysqli(
+$_ENV['OPENSHIFT_MYSQL_DB_HOST'],
+$_ENV['OPENSHIFT_MYSQL_DB_USERNAME'],
+$_ENV['OPENSHIFT_MYSQL_DB_PASSWORD'],
+$_ENV['OPENSHIFT_APP_NAME'], // By default, app name == db name
+$_ENV['OPENSHIFT_MYSQL_DB_PORT'],
+$_ENV['OPENSHIFT_MYSQL_DB_SOCKET']
+);
 
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-echo "Connected successfully<br>";
-
-$sql = "TRUNCATE orari";
-
-if (mysqli_query($conn, $sql)) {
-	echo "Table truncated<br>";
-} else {
-	echo "Error: " . $sql . "<br>" . mysqli_error($conn)."<br>";
-}
-
-echo "<br>";*/
 
 $arrary= array(10116,10232,10168,10493,10126,10128,10129,10136,10176,10178,10179,10185,10186,10187,10113,10169,10127,10130,10149,10150,10437,10175,10114,10133,10134,
 10153,10117,10131,10151,10152,10155,10156,10157,10158,10438,10159,10164,10165,10166,10167,10233,10115,10170,10112,10123,10124,10507,10137,10138,10139,10140,10234,
 10235,10055,10177);
-foreach($arrary as $corso){
+//foreach($arrary as $corso){
+	$corso=10116;
 	$homepage = file_get_contents('http://webapps.unitn.it/Orari/it/Web/AjaxEventi/c/'.$corso.'-/agendaWeek?_=1417633155537&start=1417388400&end=1417993200');
 	$o=0;
 	$json=json_decode($homepage);
 	foreach($json->Eventi as $x){
-		
 			$url=substr($x->url,1);
 			$o++;
 			$detail = file_get_contents('http://webapps.unitn.it/Orari/it/Web/DettaglioImpegno/'.$url);
@@ -69,25 +58,27 @@ foreach($arrary as $corso){
 				$tmp = array_merge(array(),$tmp);
 				for($i=-1;$i<count($tmp)-2;$i++){
 				$i++;
-		        echo "#".$o."<br>&nbsp;&nbsp;&nbsp;&nbsp;".$orain."<br>&nbsp;&nbsp;&nbsp;&nbsp;".$orafin."<br>&nbsp;&nbsp;&nbsp;&nbsp;".substr($tmp[$i],16)."<br>&nbsp;&nbsp;&nbsp;&nbsp;".$tmp[$i+1]."<br><br>";
+				$data=date("Y-m-d",$orain);
+				$sql = "INSERT INTO orario (aula, polo, orainizio, orafine,data)
+			     VALUES ('".substr($tmp[$i],16)."','".$tmp[$i+1]."',".$orain.",".$orafin.",'".$data."')";
+
+				if (mysqli_query($conn, $sql)) {
+					echo "New record created successfully<br>";
+				} else {
+						echo "Error: " . $sql . "<br>" . mysqli_error($conn)."<br>";
+				}
+		       // echo "#".$o."<br>&nbsp;&nbsp;&nbsp;&nbsp;".$orain."<br>&nbsp;&nbsp;&nbsp;&nbsp;".$orafin."<br>&nbsp;&nbsp;&nbsp;&nbsp;".substr($tmp[$i],16)."<br>&nbsp;&nbsp;&nbsp;&nbsp;".$tmp[$i+1]."<br><br>";
 
 				}
 			}
 			
-                   /*
-					$sql = "INSERT INTO orari (data, orainizio, orafine, aula, polo)
-					VALUES (".$orain.",".$orafin.", '".substr($tmp[$i],16)."', '".$tmp[$i+1]."')";
-
-					if (mysqli_query($conn, $sql)) {
-						echo "New record created successfully<br>";
-					} else {
-						echo "Error: " . $sql . "<br>" . mysqli_error($conn)."<br>";
-					}
+                   
+			
 					
-					*/
+					
 		
 	}
-}
-//mysqli_close($conn);
+//}
+mysqli_close($conn);
 echo "Secondi esecuzione: ".(time()-$iniz);
 ?>
