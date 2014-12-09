b5c2979ad911706e57f4f554223f820fd7be80d9
Rocca94 <gabriel.roccabruna@gmail.com>
2014-12-09 18:04:16 +0100

    asd

--- updates/d.php
+++ updates/d.php
@@ -1,6 +1,7 @@
 <?php
 
 $conn = new mysqli_connect(getenv('OPENSHIFT_MYSQL_DB_HOST'), getenv('OPENSHIFT_MYSQL_DB_USERNAME'), getenv('OPENSHIFT_MYSQL_DB_PASSWORD'), "", getenv('OPENSHIFT_MYSQL_DB_PORT')) or die("Error: " . mysqli_error($mysqlCon));
+echo getenv('OPENSHIFT_MYSQL_DB_HOST');
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
