371b6b0b68e607123e88864e01e0711fb9e89640
Rocca94 <gabriel.roccabruna@gmail.com>
2014-12-11 20:33:13 +0100

    ass

--- ora/req.php
+++ ora/req.php
@@ -20,25 +20,9 @@ if (!$conn) {
 	if ($result->num_rows > 0) {
 		// output data of each row
 		while($row = $result->fetch_assoc()) {
-		$sql = "SELECT DISTINCT orainizio,orafine FROM orario WHERE orario.aula='".$row["aula"]."' AND orario.polo='".$row["polo"]."' ORDER BY orario.orainizio";
-			$aulas = $conn->query($sql);
-			if ($aulas->num_rows > 0) {
-				$i=0;
-				while($col = $aulas->fetch_assoc()) {
-					$orain[$i]=$col["orainizio"];
-					$orafin[$i]=$col["orafine"];
-					$i++;
-				}
-				for($i=0;$i<count($orain);$i++){
-					if(($i+1!=count($orain)) AND ($orain[$i+1]!=$orafin[$i])){
-						$sql = "INSERT INTO freeaula (aula, polo, orainizio, orafine)
-						VALUES ('".$row["aula"]."','".$row["polo"]."',".$orafin[$i].",".$orain[$i+1].")";
-						$conn->query($sql);
-					}
-				}
+			echo $row["aula"]."/".$row["orainizio"]."/".$row["orafine"]."<removekebab>";
 			}
-			//echo "Aula:".$row["aula"]."<br>";
-		}
+     	}
 	} else {
 		echo "0 results";
 	}
